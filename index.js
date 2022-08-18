5d216136a992520875f3bb20a62248161beb51d9
MathieuSuhard <mathieusuhard.dge@gmail.com>
2022-08-18 21:51:02 +0200

    test deplyo heroku

--- index.js
+++ index.js
@@ -9,5 +9,6 @@ const PORT = process.env.PORT ?? 3000;
 const server = createServer(app);
 
 server.listen(PORT, () => {
+    console.log(`Listen on http://localhost:${PORT}`);
     debug(`http://localhost:${PORT}`);
 });
