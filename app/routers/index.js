772fbf2937d6633d0d142f3c83ca0733b9c3aa22
MathieuSuhard <mathieusuhard.dge@gmail.com>
2022-08-19 08:09:43 +0200

    test route

--- app/routers/index.js
+++ app/routers/index.js
@@ -2,12 +2,17 @@ const express = require('express');
 
 const apiRouter = require('./api');
 const websiteRouter = require('./website');
+const { userController: controller } = require('../controllers/api');
+
 const { errorHandler } = require('../helpers/errorHandler');
 
 const router = express.Router();
 
 router.use('/api', apiRouter);
 router.use('/', websiteRouter);
+router
+    .route('/test')
+    .get(controller.getAll);
 router.use(errorHandler);
 
 module.exports = router;
